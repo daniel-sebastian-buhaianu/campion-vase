1ea86afbbd6c7598343cd23301a2b0745f711411
Daniel-Sebastian Buhaianu <128343993+daniel-sebastian-buhaianu@users.noreply.github.com>
2023-11-27 11:06:49 +0200

    Update vase.cpp - scor 100

--- vase.cpp
+++ vase.cpp
@@ -1,9 +1,16 @@
 #include <fstream>
 using namespace std;
+struct Vas { int indice; Vas *urmatorul; };
+struct ListaVase { Vas *primul, *ultimul; };
+void adaugaLaInceputulListei(ListaVase &, int);
+void adaugaLaSfarsitulListei(ListaVase &, int);
 int main()
 {
 	ifstream citeste("vase.in");
 	ofstream scrie("vase.out");
+	ListaVase L;
+	L.primul = 0;
+	L.ultimul = 0;
 	int nrVase;
 	citeste >> nrVase;
 	long long
@@ -19,10 +26,12 @@ int main()
 		if (ramura == 'S')
 		{
 			gradatieStanga += volum[culoare];
+			adaugaLaInceputulListei(L, culoare);
 		}
 		else
 		{
 			gradatieDreapta += volum[culoare];
+			adaugaLaSfarsitulListei(L, culoare);
 		}
 		gradatieStanga = gradatieDreapta
 		= (gradatieStanga + gradatieDreapta)/2;
@@ -33,12 +42,15 @@ int main()
 	citeste.close();
 	scrie << gradatieStanga << endl; // gradatia maxima
 	gradatieDreapta *= -1;
-	for (culoare = nrVase-1;
-	     culoare >= culoareCautata; culoare--)
-	{
-		gradatieStanga = gradatieDreapta;
-		gradatieDreapta += volum[culoare];
- 	}
+	for (Vas *v = L.primul; v; v = v->urmatorul)
+    {
+        gradatieStanga = gradatieDreapta;
+		gradatieDreapta += volum[v->indice];
+		if (v->indice == culoareCautata)
+        {
+            break;
+        }
+    }
 	if (gradatieStanga == 0 || gradatieDreapta == 0)
 	{
 		if (gradatieDreapta == 0)
@@ -56,8 +68,8 @@ int main()
 	{
 	    if (gradatieStanga < 0 && gradatieDreapta < 0)
         {
-            scrie << gradatieDreapta*(-1) << " S\n";
             scrie << gradatieStanga*(-1) << " S";
+            scrie << gradatieDreapta*(-1) << " S\n";
         }
         else if (gradatieStanga > 0 && gradatieDreapta > 0)
         {
@@ -72,4 +84,34 @@ int main()
 	}
 	scrie.close();
 	return 0;
+}
+void adaugaLaInceputulListei(ListaVase & L, int indice)
+{
+    Vas *v = new Vas;
+    v->indice = indice;
+    v->urmatorul = 0;
+    if (!L.primul)
+    {
+        L.primul = L.ultimul = v;
+    }
+    else
+    {
+        v->urmatorul = L.primul;
+        L.primul = v;
+    }
+}
+void adaugaLaSfarsitulListei(ListaVase & L, int indice)
+{
+    Vas *v = new Vas;
+    v->indice = indice;
+    v->urmatorul = 0;
+    if (!L.ultimul)
+    {
+        L.primul = L.ultimul = v;
+    }
+    else
+    {
+        L.ultimul->urmatorul = v;
+        L.ultimul = v;
+    }
 }
